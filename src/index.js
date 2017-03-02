b22e656e5160adcf9195cc264dc98b5d28a9ba88
zjuasmn <zjuasmn@gmail.com>
2017-03-02 19:34:47 +0800

    export fs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import XFileSystem from "./XFileSystem";
 import fetchUnpkg from "./fetchUnpkg";
 
 const fs = new XFileSystem(fetchUnpkg);
+module.exports = fs;
 export default fs;
 
 exports.XFileSystem = XFileSystem;
